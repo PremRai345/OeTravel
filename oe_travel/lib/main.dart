6186a7ef5ea9cbf434c58dc888d4dc40261a41b9
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-03-05 20:47:29 +0545

    Project is Connected with Firebase
    App is connected with firebase minSdkVersion is changed to support firebase and some dependencies is added in gardels and and firebase connection is done in main dart.

--- oe_travel/lib/main.dart
+++ oe_travel/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:oe_travel/WelcomeScreen/welcome.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //Connecting to firebase
+  await Firebase.initializeApp();
   runApp(const OeTravel());
 }
 
